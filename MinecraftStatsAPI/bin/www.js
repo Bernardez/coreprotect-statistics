3c78e9123c8f079882a0898bcb0e33624d6e2072
Bernardez <bas_siegerink@hotmail.com>
2019-04-24 13:19:20 +0200

    Use HTTP instead of HTTPS for easier setup

--- MinecraftStatsAPI/bin/www.js
+++ MinecraftStatsAPI/bin/www.js
@@ -1,15 +1,11 @@
 var app = require('../app');
-var https = require('https')
-var fs = require('fs')
-var privateKey  = fs.readFileSync('/etc/letsencrypt/live/ylyl.eu/privkey.pem', 'utf8')
-var certificate = fs.readFileSync('/etc/letsencrypt/live/ylyl.eu/fullchain.pem', 'utf8')
-var credentials = {key: privateKey, cert: certificate}
+var http = require('http')
 const config = require('../config');
 var port = process.env.PORT || config.port
 
 app.set('port', port);
 
-var httpsServer = https.createServer(credentials, app)
+var httpsServer = http.createServer(app)
 
 httpsServer.listen(port)
 console.log('The magic happens on port ' + port)
